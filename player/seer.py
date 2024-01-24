c3488f4fd6044e436a4e4a5277b6c32ee95b9e24
fukufuk <122790208+fukufuk@users.noreply.github.com>
2024-01-24 12:36:47 +0900

    add: DIVINE function

--- player/seer.py
+++ player/seer.py
@@ -1,63 +1,68 @@
 import configparser
-import json
 
-import lib
+# import lib
 import player
+from lib import util
+from lib.llm.query import role_suspicion
+
+# import json
+LOGGER = util.build_logger(__name__)
 
 
 class Seer(player.agent.Agent):
     def __init__(self, inifile: configparser.ConfigParser, name: str) -> None:
-        print("init")
         super().__init__(inifile=inifile, name=name)
 
     def parse_info(self, receive: str) -> None:
-        print("parse_info")
         return super().parse_info(receive)
 
     def get_info(self):
-        print("get_info")
         return super().get_info()
 
     def initialize(self) -> None:
-        print("initialize")
         return super().initialize()
 
     def daily_initialize(self) -> None:
-        print("daily_initialize")
         return super().daily_initialize()
 
     def daily_finish(self) -> None:
-        print("daily_finish")
         return super().daily_finish()
 
     def get_name(self) -> str:
-        print("get_name")
+        
         return super().get_name()
 
     def get_role(self) -> str:
-        print("get_role")
         return super().get_role()
 
     def talk(self) -> str:
-        print("talk")
         return super().talk()
 
     def vote(self) -> str:
-        print("vote")
         return super().vote()
 
     def whisper(self) -> None:
-        print("whisper")
         return super().whisper()
 
     def divine(self) -> str:
-        print("divine")
-        data = {"agentIdx": lib.util.random_select(self.alive)}
+        LOGGER.info(f"[{self.name}] DIVINE")
+        role_suspicion_text = role_suspicion(self.agent_role_suspect)
+        latest_talks = "\n".join(
+            [f'Agent[0{talk["agent"]}]: {talk["text"]}'
+             for talk in self.todays_talk_history])
+        arguments = self.client.divine(
+            game_setting=self.game_rule,
+            game_info=self.game_info_text,
+            role_suspicion=role_suspicion_text,
+            talkHistory=latest_talks
+        )
+        LOGGER.info(f"[{self.name}] DIVINE end")
+        return arguments
+        # data = {"agentIdx": lib.util.random_select(self.alive)}
 
-        return json.dumps(data, separators=(",", ":"))
+        # return json.dumps(data, separators=(",", ":"))
 
     def action(self) -> str:
-        print("action")
         if self.request == "DIVINE":
             return self.divine()
         else:
