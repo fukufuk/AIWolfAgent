9369dacf99c02db7183d6c9c821fa726b63203f9
fukufuk <122790208+fukufuk@users.noreply.github.com>
2024-03-16 14:14:06 +0900

    change initial.py

--- initial.py
+++ initial.py
@@ -9,17 +9,17 @@ if __name__ == "__main__":
                       local_dir="embedding_model")
     
     # 類似度検索をするためのデータ生成
-    embedding = Embedding()
-    players = ["私", "Agent[01]", "Agent[02]", "Agent[03]", "Agent[04]", "Agent[05]"]
-    roles = ["人狼", "村人", "狂人", "占い師", "白", "黒"]
-    txts = ["{}は{}だ", "{}は{}ではない"]
-    examples = []
-    for txt in txts:
-        for a in players:
-            for b in roles:
-                examples.append(txt.format(a, b))
-    script_embedding = map(embedding.encode, examples)
-    script_embedding = list(script_embedding)
+    # embedding = Embedding()
+    # players = ["私", "Agent[01]", "Agent[02]", "Agent[03]", "Agent[04]", "Agent[05]"]
+    # roles = ["人狼", "村人", "狂人", "占い師", "白", "黒"]
+    # txts = ["{}は{}だ", "{}は{}ではない"]
+    # examples = []
+    # for txt in txts:
+    #     for a in players:
+    #         for b in roles:
+    #             examples.append(txt.format(a, b))
+    # script_embedding = map(embedding.encode, examples)
+    # script_embedding = list(script_embedding)
 
-    with open('lib/embedding/data/script_emb.pickle', 'wb') as f:
-        pickle.dump([examples, script_embedding], f)
+    # with open('lib/embedding/data/script_emb.pickle', 'wb') as f:
+    #     pickle.dump([examples, script_embedding], f)
